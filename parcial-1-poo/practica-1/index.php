67332c31797443e3b06b7f9dd548d7eef917f7e0
NateStar20 <jose.gerardito.sanchez@gmail.com>
2026-03-05 23:02:35 -0700

    Pureba de la clase

--- parcial-1-poo/practica-1/index.php
+++ parcial-1-poo/practica-1/index.php
@@ -1,1 +1,13 @@
 <?php
+include 'Usuario.php'; //Se incluye la clase para instanciarla
+
+$objUsuario1 = new Usuario ("Gerardo Sanchez", "gerardosr118@gmail.com"); //Instancia de la clase Usuario
+
+echo '<p> Nombre: ' . $objUsuario1->getNombre() . '</p>';   
+echo '<p> Correo: ' . $objUsuario1->getCorreo() . '</p>';
+
+
+$objUsuario1->setNombre("Jose Rodriguez");  //Modificacion de la variable nombre 
+
+echo '<p> Nombre: ' . $objUsuario1->getNombre() . '</p>';
+echo '<p> Correo: ' . $objUsuario1->getCorreo() . '</p>';
