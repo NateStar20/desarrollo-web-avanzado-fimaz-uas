9d69da9765d178fe5cae398234e26fa1d8e86c6b
NateStar20 <jose.gerardito.sanchez@gmail.com>
2026-03-05 21:22:32 -0700

    Implementacion de la clase Usuario

--- parcial-1-poo/practica-1/Usuario.php
+++ parcial-1-poo/practica-1/Usuario.php
@@ -1,1 +1,32 @@
 <?php
+ class Usuario {
+
+    private $vNombre;
+    private $vCorreo;
+
+    public function __construct($nombre,$correo)
+    {
+        $this->vNombre=$nombre;
+        $this->vCorreo=$correo;    
+    }
+
+    public function getNombre() 
+    {
+        return $this->vNombre;
+    }
+
+    public function getCorreo()
+    {
+        return $this->vCorreo;
+    }
+
+    public function setNombre()
+    {
+        return $this->vNombre;
+    }
+
+    public function setCorreo()
+    {
+        return $this->vCorreo;
+    }
+}
