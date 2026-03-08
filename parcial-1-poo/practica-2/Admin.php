90bd3007485d11d8e05bab685516155375ab296a
NateStar20 <jose.gerardito.sanchez@gmail.com>
2026-03-08 11:21:18 -0700

    Realizamos el codigo de la clase admin que va a herederar de la clase usuario

--- parcial-1-poo/practica-2/Admin.php
+++ parcial-1-poo/practica-2/Admin.php
@@ -1,2 +1,17 @@
 <?php
+/**
+ * Clase Admin que hereda de Usuario.
+ * @package Usuarios
+ */
+require 'Usuario.php';
 
+class Admin extends Usuario {
+    /**
+     * Retorna el rol específico de esta clase.
+     * @return string
+     */
+    public function getRol() 
+    {
+        return "Administrador";
+    }
+}
