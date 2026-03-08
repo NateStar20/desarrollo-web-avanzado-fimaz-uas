fedc1db0af8d067f0d38b21aeb835eebe87c95fd
NateStar20 <jose.gerardito.sanchez@gmail.com>
2026-03-08 14:54:46 -0700

    Implementacion del codigo de todos los archivos

--- parcial-1-poo/practica-3/clases/Admin.php
+++ parcial-1-poo/practica-3/clases/Admin.php
@@ -1,1 +1,22 @@
 <?php
+
+require_once 'Usuario.php';
+
+/**
+ * Clase que representa a un Administrador dentro del sistema.
+ * Hereda la estructura y las validaciones de la clase base Usuario.
+ * @package Usuarios
+ * @author Jose Gerardo Sanchez Rodriguez
+ */
+class Admin extends Usuario {
+
+    /**
+     * Obtiene el rol del usuario en el sistema.
+     * Define el comportamiento específico para el perfil de Administrador.
+     * @return string Retorna la cadena 'Administrador'.
+     */
+    public function getRol()
+    {
+        return 'Administrador';
+    }
+}
