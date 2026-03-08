2295642ad0c8696fe6bb65b139de186066754b85
NateStar20 <jose.gerardito.sanchez@gmail.com>
2026-03-08 11:32:35 -0700

    Aqui instanciamos para la parte de la prueba

--- parcial-1-poo/practica-2/index.php
+++ parcial-1-poo/practica-2/index.php
@@ -1,2 +1,11 @@
 <?php
 
+require 'Admin.php';
+
+$objAdmin1 = new Admin ("Gerardo Sanchez", "gerardosr118@gmail.com");
+
+echo '<p> Nombre: ' . $objAdmin1->getNombre() . '</p>';
+echo '<p> Correo: ' . $objAdmin1->getCorreo() . '</p>';
+echo '<p> Rol: ' . $objAdmin1->getRol() . '</p>';
+
+?>
